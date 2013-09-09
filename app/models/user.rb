9766fb505a55ecb28c4ce30cf58c342047c1a9da
Dave Thompson <davethompson21@gmail.com>
2013-09-09 14:29:19 +0100

    Adding test and improving validation of users email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,12 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 200 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 end
